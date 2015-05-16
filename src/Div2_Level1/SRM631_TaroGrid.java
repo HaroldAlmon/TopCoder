09d8f3257c786fc3bfa74c4dbd2647d89f0a8bb3
haroldalmon <haroldemail@gmail.com>
2015-05-16 17:48:19 -0500

    Fix bug and refactor file.

--- src/Div2_Level1/SRM631_TaroGrid.java
+++ src/Div2_Level1/SRM631_TaroGrid.java
@@ -1,5 +1,6 @@
 package Div2_Level1;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -7,44 +8,49 @@ import org.junit.Test;
 public class SRM631_TaroGrid {
 	public int getNumber(String[] grid) {
 		int result = 0;
-		result = Math.max(maxX(grid), maxY(grid) );
+		result = Math.max(maxInRows(grid), maxInColumns(grid) );
 		return result;
 	}
 	
-	// find max W or B count in a string...
-	private int maxX(String[] grid) {
+	private int maxInRows(String[] grid) {
 		int max = 0;
-		for (int i = 0; i < grid.length; i++) {
-			int countW;
-			int countB;
-			countW = 0;
-			countB = 0;
-			char[] ga = grid[i].toCharArray();
-			for (int j = 0; j < ga.length; j++) {
-				if(ga[j] == 'W') {
-					countW++;
-				} else
-					countB++;
+		for (int row = 0; row < grid.length; row++) {
+			int whiteCount;
+			int blackCount;
+			whiteCount = 0;
+			blackCount = 0;
+			char[] letters = grid[row].toCharArray();
+			for (int rowPosition = 0; rowPosition < letters.length; rowPosition++) {
+				if(letters[rowPosition] == 'W') {
+					whiteCount++;
+					blackCount = 0;
+				} else {
+					blackCount++;
+					whiteCount = 0;
+				}
 			}
-			max = Math.max(max, Math.max(countW, countB));
+			max = Math.max(max, Math.max(whiteCount, blackCount));
 		}
 		return max;
 	}
 	
-	private int maxY(String[] grid) {
+	private int maxInColumns(String[] grid) {
 		int max = 0;
-		for (int i = 0; i < grid[0].length(); i++) {
-			int countW;
-			int countB;
-			countW = 0;
-			countB = 0;
-			for (int j = 0; j < grid.length; j++) {
-				if(grid[j].charAt(i) == 'W') {
-					countW++;
-				} else
-					countB++;				
+		for (int column = 0; column < grid[0].length(); column++) {
+			int whiteCount;
+			int blackCount;
+			whiteCount = 0;
+			blackCount = 0;
+			for (int row = 0; row < grid.length; row++) {
+				if(grid[row].charAt(column) == 'W') {
+					whiteCount++;
+					blackCount = 0;
+				} else {
+					blackCount++;
+					whiteCount = 0;
+				}
 			}
-			max = Math.max(max, Math.max(countW, countB));
+			max = Math.max(max, Math.max(whiteCount, blackCount));
 		}
 		return max;
 	}
@@ -52,31 +58,38 @@ public class SRM631_TaroGrid {
 	@Test
 	public void test1() {
 		String[] a = {"W"};
-		assertTrue( getNumber(a)  == 1);
+		assertEquals( 1, getNumber(a) );
 		
 		String[] b = {
 				"WB",
 				"BW"};
-		assertTrue( getNumber(b)  == 1);
+		assertEquals( 1, getNumber(b) );
 		
 		String[] c = {
 				"BWW",
 				"BBB",
 				"BWB"};
-		assertTrue( getNumber(c)  == 3);
+		assertEquals( 3, getNumber(c) );
 		
 		String[] d = {
 				"BWBW",
 				"BBWB",
 				"WWWB",
 				"BWWW"};
-		assertTrue( getNumber(d)  == 3);
+		assertEquals( 3, getNumber(d) );
 		
 		String[] e = {
 				"BWBW",
 				"BWWB",
 				"WWWB",
 				"BWWW"};
-		assertTrue( getNumber(e)  == 4);
+		assertEquals( 4, getNumber(e) );
+		
+		String[] f = {
+				"BWBW",
+				"BWWB",
+				"WBBW",
+				"BWBW"};
+		assertEquals( 2, getNumber(f) );
 	}
 }
