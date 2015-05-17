d1116441dc1db68d8c59284b007fe5786c1a3238
haroldalmon <haroldemail@gmail.com>
2015-05-17 06:19:43 -0500

    Rename variables, refactor JUnit tests

--- src/Div2_Level1/SRM635_IdentifyingWood.java
+++ src/Div2_Level1/SRM635_IdentifyingWood.java
@@ -5,37 +5,43 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 public class SRM635_IdentifyingWood {
-	private String yes = "Yep, it's wood";
-	private String no = "Nope";
-	public String check(String s, String t) {
+	private String YES = "Yep, it's wood";
+	private String NO = "Nope";
 
-		String result = no;
+	public String check(String s, String t) {
+		String result = NO;
 		
-		int j = 0;
-		int i = 0;
-		boolean found = false;
-		for (; i < t.length(); i++) {
+		// Create aliases for s and t...
+		String sourceDigits = s;
+		String targetDigits = t;
+		
+		int sourcePos = 0;
+		int targetPos = 0;
+
+		for (; targetPos < targetDigits.length(); targetPos++) {
+			boolean found;
+
 			found = false;
-			for (; j < s.length(); j++) {
-				if(t.charAt(i) == s.charAt(j)) {
+			for (; sourcePos < sourceDigits.length(); sourcePos++) {
+				if(targetDigits.charAt(targetPos) == sourceDigits.charAt(sourcePos)) {
 					found = true;
-					j++;
+					sourcePos++;
 					break;		
 				}
 			}
 			if (found == false) 
 				break;
 		}
-		result = (i == t.length()) ? yes : no;
+		result = (targetPos == targetDigits.length()) ? YES : NO;
 		return result;
 	}
 
 	@Test
 	public void test1() {	
-		assertTrue( check("absdefgh", "asdf").equals(yes) );
-		assertTrue( check("oxoxoxox", "ooxxoo").equals(no) );
-		assertTrue( check("oxoxoxox", "xxx").equals(yes) );
-		assertTrue( check("qwerty", "qwerty").equals(yes) );
-		assertTrue( check("string", "longstring").equals(no) );
+		assertTrue( check("absdefgh", "asdf").equals(YES) );
+		assertTrue( check("oxoxoxox", "ooxxoo").equals(NO) );
+		assertTrue( check("oxoxoxox", "xxx").equals(YES) );
+		assertTrue( check("qwerty", "qwerty").equals(YES) );
+		assertTrue( check("string", "longstring").equals(NO) );
 	}
 }
