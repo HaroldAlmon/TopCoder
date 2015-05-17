11d40478c8010633b2b794b72d1a3a9595e12b18
haroldalmon <haroldemail@gmail.com>
2015-05-17 08:10:26 -0500

    Refactor.

--- src/Div2_Level1/SRM636_GameOfStones.java
+++ src/Div2_Level1/SRM636_GameOfStones.java
@@ -5,35 +5,30 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class SRM636_GameOfStones {
-	public static void main(String[] args) {
-		SRM636_GameOfStones main = new SRM636_GameOfStones();
-		main.test1();
-	}
-	
 	public int count(int[] stones) {
 		int actionsTotal = 0;
 		int stonesSum = 0;
+		int movesTotal = 0;
 
 		stonesSum = sumOfStones( stones );
 
 		if ( isStonesSumValid( stones.length, stonesSum ) == false )
 			return -1;
-		
+
 		int targetSum = stonesSum / stones.length;
-		
+
 		for ( int stonePos = 0; stonePos < stones.length; stonePos++ ) {
-			actionsTotal += Math.abs( movesRequiredToReachTargetCount(targetSum, stones[ stonePos]  ) );
+			movesTotal += Math.abs( movesRequiredToReachTargetCount( targetSum, stones[ stonePos]  ) );
 		}
 
-		// Each move adjusts the count on 2 piles, so cut the moves in half...
-		actionsTotal = movesToActions(actionsTotal);
-
+		actionsTotal = movesToActions( movesTotal );
 		System.out.printf( "%d%n", actionsTotal );
 		return actionsTotal;
 	}
 
-	private int movesToActions(int actionsTotal) {
-		return actionsTotal / 2;
+	private int movesToActions(int movesTotal) {
+		// Each move on one pile results in a second move on another pile so number of Actions = moves / 2
+		return movesTotal / 2;
 	}
 
 	private int movesRequiredToReachTargetCount(int targetSum, int pileCount) {
@@ -59,12 +54,12 @@ public class SRM636_GameOfStones {
 	}
 
 	@Test
-	public void test1() {	
+	public void GameOfStones() {	
 		int[] a = {7, 15, 9, 5};
 		assertEquals( 3, count(a) );
 
 		int[] b = {17};
-		assertTrue( count(b) == 0 );
+		assertEquals( 0, count(b) );
 		
 		int[] c = {10, 16};
 		assertTrue( count(c) == -1 );
