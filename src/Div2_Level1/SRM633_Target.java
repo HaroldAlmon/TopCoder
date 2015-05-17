4a63594f03f440d15de7f67ff96f3e5eff07cb1f
haroldalmon <haroldemail@gmail.com>
2015-05-17 06:18:14 -0500

    Split main function into smaller functions

--- src/Div2_Level1/SRM633_Target.java
+++ src/Div2_Level1/SRM633_Target.java
@@ -1,40 +1,59 @@
 package Div2_Level1;
 
 import java.util.Arrays;
-
 import org.junit.Test;
 
 public class SRM633_Target {
 	public String[] draw(int n) {
-		String result[] = new String[n];
-		int size = n/2 + 1;
-		char[][] grid = new char[n][n];
-		int fillSize;
-		for (int i = 0; i < size; i+=2) {
-			fillSize = n-2*i;
-			for (int j = i; j < i+fillSize; j++) {
-				grid[i][j] = '#';
-				grid[i+fillSize-1][j] = '#';
-			}
-			for (int row = i+1; row < i+fillSize-1; row++) {
-				grid[row][i] = '#';
-				grid[row][i+fillSize-1] = '#';
-			}
-		}
-		StringBuilder line = new StringBuilder();
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++)
-				line.append( grid[i][j] );
-			result[i] = line.toString();
-			line.setLength(0);
-			System.out.printf("%s\n", result[i]);
-		}
+		int targetSize = n;
+		String paperTarget[] = new String[ targetSize ];
+		int centrePos = targetSize / 2 + 1;
+		char[][] grid = new char[ targetSize ][ targetSize ];
+
+		drawDecreasingRectangles(targetSize, centrePos, grid);
+		printTarget(targetSize, paperTarget, grid);
 		
-		return result;
+		return paperTarget;
 	}
+
+	private void printTarget(int targetSize, String[] paperTarget, char[][] grid) {
+		StringBuilder line = new StringBuilder();
+
+		for (int row = 0; row < targetSize; row++) {
+			for (int column = 0; column < targetSize; column++)
+				line.append( grid[ row ][ column ] );
+			paperTarget[ row ] = line.toString();
+			line.setLength( 0 );
+			System.out.printf("%s%n", paperTarget[ row ]);
+		}
+	}
+
+	private void drawDecreasingRectangles(int targetSize, int centrePos, char[][] grid) {
+		int fillSize;
+
+		for (int origin = 0; origin < centrePos; origin += 2) {
+			fillSize = targetSize - 2 * origin;
+			drawTopAndBottomLines(fillSize, grid, origin);
+			drawLeftAndRightLines(fillSize, grid, origin);
+		}
+	}
+
+	private void drawLeftAndRightLines(int fillSize, char[][] grid, int origin) {
+		for (int row = origin + 1; row < origin + fillSize - 1; row++) {
+			grid[ row ][ origin ] = '#';
+			grid[ row ][ origin + fillSize - 1 ] = '#';
+		}
+	}
+
+	private void drawTopAndBottomLines(int fillSize, char[][] grid, int origin) {
+		for (int column = origin; column < origin + fillSize; column++) {
+			grid[ origin ][ column ] = '#';
+			grid[ origin + fillSize - 1 ][ column ] = '#';
+		}
+	}
+
 	@Test
-	public void test1() {
-		//draw(5);
-		draw(21);
+	public void Target() {
+		draw(9);
 	}
 }
