dd976f063f7cb8e1311674bbd03e6c7e60dff642
haroldalmon <haroldemail@gmail.com>
2015-05-17 06:10:26 -0500

    Rename variables, refactor JUnit tests

--- src/Div2_Level1/SRM632_RunningAroundPark.java
+++ src/Div2_Level1/SRM632_RunningAroundPark.java
@@ -1,34 +1,35 @@
 package Div2_Level1;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
 public class SRM632_RunningAroundPark {
 	public int numberOfLap(int N, int[] d) {
-		int result = 1;
-		int last;
-		last = d[0];
-		for (int i = 1; i < d.length; i++) {
-			if(d[i] <= last)
-				result++;
-			last = d[i];
+		int laps = 1;
+		int lastTree;
+		lastTree = d[0];
+		for (int treePosition = 1; treePosition < d.length; treePosition++) {
+			if(d[treePosition] <= lastTree)
+				laps++;
+			lastTree = d[treePosition];
 		}
-		System.out.printf("result=%d\n", result);
-		return result;
+		System.out.printf("result=%d%n", laps);
+		return laps;
 	}
+
 	@Test
-	public void test1(){
+	public void RunningAroundPark(){
 		int[] a = {1,2,3};
-		assertTrue( numberOfLap(3, a) == 1);
+		assertEquals( 1, numberOfLap(3, a) );
 		
 		int[] b = {6,6};
-		assertTrue( numberOfLap(3, b) == 2);
+		assertEquals( 2, numberOfLap(3, b) );
 		
 		int[] c = {3,2,1};
-		assertTrue( numberOfLap(3, c) == 3);
+		assertEquals( 3, numberOfLap(3, c) );
 		
 		int[] d = {1,3,5,7,9,2,4,6,8,10};
-		assertTrue( numberOfLap(3, d) == 2);
+		assertEquals( 2, numberOfLap(3, d) );
 	}
 }
