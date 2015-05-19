384b44dc9b6f53ad437843248af82b807f291b9d
haroldalmon <haroldemail@gmail.com>
2015-05-19 05:50:59 -0500

    Refactor

--- src/Div2_Level1/SRM639_ElectronicPetEasy.java
+++ src/Div2_Level1/SRM639_ElectronicPetEasy.java
@@ -8,14 +8,15 @@ import java.util.Set;
 import org.junit.Test;
 
 public class SRM639_ElectronicPetEasy {
-	String isDifficult(int st1, int p1, int t1, int st2, int p2, int t2) {
+	String isDifficult( int st1, int p1, int t1, int st2, int p2, int t2 ) {
 		Set<Integer> feedingTime = new HashSet<>();
-		for( int i=st1; i < st1 + p1 * t1; i += p1) {
-			feedingTime.add(i);
-			//System.out.printf("Added %d\n", i);
+
+		for( int i = st1; i < st1 + p1 * t1; i += p1 ) {
+			feedingTime.add( i );
 		}
-		for( int i=st2; i < st2 + p2 * t2; i += p2) {
-			if( feedingTime.contains(i) ){
+
+		for( int i = st2; i < st2 + p2 * t2; i += p2 ) {
+			if( feedingTime.contains( i ) ){
 				return "Difficult";
 			}
 		}
@@ -23,7 +24,7 @@ public class SRM639_ElectronicPetEasy {
 	}
 	
 	@Test
-	public void test1() {
+	public void ElectronicPetEasy() {
 		assertTrue(isDifficult(3, 3, 3, 5, 2, 3) == "Difficult");
 		assertTrue(isDifficult(3, 3, 3, 5, 2, 2) == "Easy");
 		assertTrue(isDifficult(1, 4, 6, 1, 4, 7) == "Difficult");
