119347b537f3575644b68af7026841d9b094bb07
haroldalmon <haroldemail@gmail.com>
2015-05-28 17:33:21 -0500

    Initial version.

---
+++ src/Div2_Level1/SRM648_KitayutaMart.java
@@ -0,0 +1,35 @@
+package Div2_Level1;
+
+//SRM 648 Div 2 - 250 Point
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SRM648_KitayutaMart {
+	public int numBought(int K, int T) {
+		System.out.printf("Total = %d Apples Count = %d\n", T,  (int)(Math.log((double)(T+K)/K) / Math.log(2.0)));
+		return (int)(Math.log((T+K)/K) / Math.log(2.0));
+	}
+	
+	@Test
+	public void test1() {
+		assertTrue(numBought(100,100) == 1);
+	}
+	
+	@Test
+	public void test2() {
+		assertTrue(numBought(100,300) == 2);
+	}
+	
+	@Test
+	public void test3() {
+		assertTrue(numBought(150, 1050) == 3);
+	}
+	
+	@Test
+	public void test4() {
+		assertTrue(numBought(160, 163680) == 10);
+	}
+}
+
+
