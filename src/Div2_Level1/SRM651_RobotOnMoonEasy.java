74c8c17e7bde0dc9287b934c83f9dfb65180405a
haroldalmon <haroldemail@gmail.com>
2015-05-29 18:41:45 -0500

    Refactor

--- src/Div2_Level1/SRM651_RobotOnMoonEasy.java
+++ src/Div2_Level1/SRM651_RobotOnMoonEasy.java
@@ -3,62 +3,60 @@ package Div2_Level1;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
-//===== DO NOT COPY ^^^^ ==================================
-
 public class SRM651_RobotOnMoonEasy {
 	public String isSafeCommand(String[] board, String S) {
-		String result = "Alive";
-		int strPos = 0;
+		String robotHealth;
+		int digitIdx = 0;
 		int row = 0;
 		
 		// Find the robot...
 		for (int i = 0; i < board.length; i++) {
 			if (board[i].indexOf('S') >= 0) {
-				strPos = board[i].indexOf('S');
+				digitIdx = board[i].indexOf('S');
 				row = i;
+				break;
 			}
-			//System.out.printf("array = %s\n", board[i]);
 		}
-		//System.out.printf("start (%d, %d)\n", strPos, row);
 
+		robotHealth = moveRobot(board, S, digitIdx, row);
+		
+		//System.out.printf(" Result = %s%d", result);
+		return robotHealth;
+		
+	}
+
+	private String moveRobot(String[] board, String S, int digitIdx, int row) {
 		// Move the Robot...
 		for (int i = 0; i < S.length(); i++) {
 			switch (S.charAt(i)) {
 			case 'U':
-				if (row == 0 || board[row - 1].charAt(strPos) != '#')
+				if (row == 0 || board[row - 1].charAt(digitIdx) != '#')
 					row--;
 				break;
 
 			case 'D':
-				if ((row == board.length - 1) || (board[row + 1].charAt(strPos) != '#'))
+				if ((row == board.length - 1) || (board[row + 1].charAt(digitIdx) != '#'))
 					row++;
 				break;
 
 			case 'L':
-				if (strPos == 0 || board[row].charAt(strPos - 1) != '#')
-					strPos--;
+				if (digitIdx == 0 || board[row].charAt(digitIdx - 1) != '#')
+					digitIdx--;
 				break;
 
 			case 'R':
-				if ((strPos == board[0].length() - 1) || (board[row].charAt(strPos + 1) != '#'))
-					strPos++;
+				if ((digitIdx == board[0].length() - 1) || (board[row].charAt(digitIdx + 1) != '#'))
+					digitIdx++;
 			}
 
 			// Check if stPos or row goes off the board...
-			if (strPos < 0 || strPos >= board[0].length() || row < 0
+			if (digitIdx < 0 || digitIdx >= board[0].length() || row < 0
 					|| row >= board[0].length())
 				return "Dead";
-
-			// System.out.printf("cmd = %s, position (%d, %d)\n", S.charAt(i),
-			// strPos, row);
 		}
-		
-		//System.out.printf(" Result = %s\n", result);
-		return result;
-		
+		return "Alive";
 	}
 	
-	// DO NOT COPY ==== vvvvv =====
 	@Test
 	public void test0() {
 		String[] board = 		
@@ -69,7 +67,6 @@ public class SRM651_RobotOnMoonEasy {
 		String S = "URURURURUR";
 		
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Alive") ;
 	}
 	
@@ -83,7 +80,6 @@ public class SRM651_RobotOnMoonEasy {
 		String S = "URURURURUR";
 		
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Dead") ;
 	}
 	
@@ -97,7 +93,6 @@ public class SRM651_RobotOnMoonEasy {
 		String S = "URURU";
 		
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Alive") ;
 	}
 
@@ -112,7 +107,6 @@ public class SRM651_RobotOnMoonEasy {
 				"#####" };
 		String S = "DRULURLDRULRUDLRULDLRULDRLURLUUUURRRRDDLLDD";
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Alive") ;
 	}
 	
@@ -126,7 +120,6 @@ public class SRM651_RobotOnMoonEasy {
 				 "#.###"};
 		String S = "DRULURLDRULRUDLRULDLRULDRLURLUUUURRRRDDLLDD";
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Dead") ;
 	}
 	
@@ -137,7 +130,6 @@ public class SRM651_RobotOnMoonEasy {
 				{"S"};
 		String S = "R";
 		String result = isSafeCommand(board, S);
-		//System.out.println("result = " + result);
 		assertTrue( result == "Dead") ;
 	}	
 }
