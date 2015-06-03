6f709775d08945a607698ee565c5d482503b86cb
haroldalmon <haroldemail@gmail.com>
2015-06-03 06:17:07 -0500

    Add unit test

--- src/Div2_Level1/SRM636_GameOfStones.java
+++ src/Div2_Level1/SRM636_GameOfStones.java
@@ -3,7 +3,7 @@ package Div2_Level1;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
-
+/** Passed TopCoder System Test 05/29/2015 */
 public class SRM636_GameOfStones {
 	public int count(int[] stones) {
 		int actionsTotal = 0;
@@ -12,30 +12,19 @@ public class SRM636_GameOfStones {
 
 		stonesSum = sumOfStones( stones );
 
-		if ( isStonesSumValid( stones.length, stonesSum ) == false )
+		if ( stonesSum % stones.length != 0 ) {
 			return -1;
-
+		}
 		int targetSum = stonesSum / stones.length;
-
+		
 		for ( int stonePos = 0; stonePos < stones.length; stonePos++ ) {
-			movesTotal += Math.abs( movesRequiredToReachTargetCount( targetSum, stones[ stonePos]  ) );
+			movesTotal += Math.abs( ( targetSum - stones[ stonePos]  ) / 2 );
+			if ( (targetSum - stones[ stonePos]) % 2 != 0) 
+				return -1;
 		}
 
 		actionsTotal = movesToActions( movesTotal );
-		System.out.printf( "%d%n", actionsTotal );
 		return actionsTotal;
-	}
-
-	private boolean isStonesSumValid(int numberOfPiles, int stonesSum) {
-		if( numberOfPiles == 1) 
-			return true;
-		if( stonesSum % 4 != 0) 
-			return false;
-		return true;
-	}
-
-	private int movesRequiredToReachTargetCount(int targetSum, int pileCount) {
-		return ( targetSum - pileCount  ) / 2;
 	}
 
 	private int movesToActions(int movesTotal) {
@@ -46,8 +35,8 @@ public class SRM636_GameOfStones {
 	private int sumOfStones(int[] stones) {
 		int stonesSum = 0;
 
-		for ( int i = 0; i < stones.length; i++ ) {
-			stonesSum += stones[ i ];
+		for ( int position = 0; position < stones.length; position++ ) {
+			stonesSum += stones[ position ];
 		}
 
 		return stonesSum;
@@ -65,13 +54,21 @@ public class SRM636_GameOfStones {
 		assertTrue( count(c) == -1 );
 		
 		int[] d = {2, 6};
-		assertTrue( count(d) == 1 );
+		assertEquals( 1, count(d) );
 		
 		int[] e = {2, 8, 4};
-		assertTrue( count(e) == -1 );
+		assertEquals( -1, count(e) );
 		
 		int[] f = {10, 15, 20, 12, 1, 20};
 		assertTrue( count(f) == -1 );
+		
+
+	}
+	@Test
+	public void test2() {
+		int[] g =
+		{17, 1, 27, 29, 13, 1, 27, 3, 19, 3, 25, 1, 11, 9, 7, 17, 31, 25, 5, 11, 31, 9, 15, 3, 3, 3, 11, 11, 1, 41, 5, 95, 7, 3, 41, 31, 7, 13, 15, 5, 17, 3, 9, 3, 11, 27, 1, 23, 15, 5, 43, 11, 17, 7, 1, 3, 13, 69, 3, 43, 21, 1, 25, 1, 3, 11, 5, 43, 13, 7, 15, 1, 1, 55, 37, 9, 5, 7, 21, 3, 23, 15, 1, 9, 3, 35, 13, 17, 7, 17, 27, 5, 9, 19, 13, 1, 1, 1, 29};
+		assertEquals( 277, count(g) );
 	}
 
 }
