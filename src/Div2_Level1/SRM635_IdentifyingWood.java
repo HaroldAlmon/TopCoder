a096a04306f89625e099a2ff238ca4f5ae3b44ea
haroldalmon <haroldemail@gmail.com>
2015-06-05 08:10:37 -0500

    add unit tests

--- src/Div2_Level1/SRM635_IdentifyingWood.java
+++ src/Div2_Level1/SRM635_IdentifyingWood.java
@@ -38,10 +38,26 @@ public class SRM635_IdentifyingWood {
 
 	@Test
 	public void IdentifyingWood() {	
-		assertTrue( check("absdefgh", "asdf").equals(YES) );
+		assertTrue( check("string", "longstring").equals(NO) );
+	}
+	
+	@Test
+	public void IdentifyingWood2() {	
 		assertTrue( check("oxoxoxox", "ooxxoo").equals(NO) );
+	}
+	
+	@Test
+	public void IdentifyingWood3() {	
 		assertTrue( check("oxoxoxox", "xxx").equals(YES) );
+	}
+	
+	@Test
+	public void IdentifyingWood4() {	
 		assertTrue( check("qwerty", "qwerty").equals(YES) );
+	}
+	
+	@Test
+	public void IdentifyingWood5() {	
 		assertTrue( check("string", "longstring").equals(NO) );
 	}
 }
