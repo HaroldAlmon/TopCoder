f34f893e7d725eaeadeace1074bb13195b19d291
haroldalmon <haroldemail@gmail.com>
2015-06-18 07:36:53 -0500

    Format data

--- src/JUnitTests/SRM645_BacteriesColonyTest.java
+++ src/JUnitTests/SRM645_BacteriesColonyTest.java
@@ -21,10 +21,10 @@ public class SRM645_BacteriesColonyTest {
 @Parameters
 public static List<Object []> data() {
 	Object [][] data = {
-			{ new int[] {5, 3, 4, 6, 1 }, new int[]  {5, 4, 4, 4, 1 } },
-			{ new int[] { 1, 5, 4, 9 }, new int[] { 1, 4, 5, 9 } },
-			{ new int[] { 78, 34, 3, 54, 44, 99 },	new int[] { 78, 34, 34, 49, 49, 99 }},
-			{ new int[] {78, 34, 3, 54, 44, 99 }, new int[] {78, 34, 34, 49, 49, 99 }}
+		{ new int[] { 5, 3, 4, 6, 1 },          new int[] { 5, 4, 4, 4, 1 } },
+		{ new int[] { 1, 5, 4, 9 },             new int[] { 1, 4, 5, 9 } },
+		{ new int[] { 78, 34, 3, 54, 44, 99 },	new int[] { 78, 34, 34, 49, 49, 99 } },
+		{ new int[] { 78, 34, 3, 54, 44, 99 },  new int[] { 78, 34, 34, 49, 49, 99 } }
 	};
 	return Arrays.asList(data);
 }
