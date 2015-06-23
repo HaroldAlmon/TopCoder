68244e2925531b768de8d975ba53cfb17ad87e9e
haroldalmon <haroldemail@gmail.com>
2015-06-23 06:31:14 -0500

    Delete JUnit tests

--- src/Div2_Level1/SRM637_GreaterGameDiv2.java
+++ src/Div2_Level1/SRM637_GreaterGameDiv2.java
@@ -13,44 +13,4 @@ public class SRM637_GreaterGameDiv2 {
 		}
 		return result;
 	}
-
-	@Test
-	public void test1() {
-		int[] a = {1,3};
-		int[] b = {4,2};
-		assertTrue( calc(a, b) == 1);
-
-		int[] c = {1,3,5,7,9};
-		int[] d = {2,4,6,8,10};
-		assertTrue( calc(c, d) == 0);
-		
-		int[] e = {2};
-		int[] f = {1};
-		assertTrue( calc(e, f) == 1);
-
-		int[] g = {3,5,9,16,14,20,15,17,13,2};
-		int[] h = {6,18,1,8,7,10,11,19,12,4};
-		assertTrue( calc(g, h) == 6);
-	}
-
-	@Test
-	public void test2() {
-		int[] c = {1,3,5,7,9};
-		int[] d = {2,4,6,8,10};
-		assertTrue( calc(c, d) == 0);
-	}
-	
-	@Test
-	public void test3() {
-		int[] e = {2};
-		int[] f = {1};
-		assertTrue( calc(e, f) == 1);
-	}
-	
-	@Test
-	public void test4() {
-		int[] g = {3,5,9,16,14,20,15,17,13,2};
-		int[] h = {6,18,1,8,7,10,11,19,12,4};
-		assertTrue( calc(g, h) == 6);
-	}
 }
