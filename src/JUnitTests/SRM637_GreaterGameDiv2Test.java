518cd90f67bb541ed8b4b946030bc7e00387be9c
haroldalmon <haroldemail@gmail.com>
2015-06-29 17:51:35 -0500

    Add unit tests

--- src/JUnitTests/SRM637_GreaterGameDiv2Test.java
+++ src/JUnitTests/SRM637_GreaterGameDiv2Test.java
@@ -1,4 +1,6 @@
 package JUnitTests;
+import static org.junit.Assert.assertEquals;
+
 import java.util.Arrays;
 import java.util.List;
 
@@ -7,48 +9,38 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 import org.junit.runners.Parameterized.Parameters;
 
+import Div2_Level1.SRM637_GreaterGameDiv2;
+
 @RunWith(Parameterized.class)
 public class SRM637_GreaterGameDiv2Test {
 	int cardListSnuke[];
 	int cardListSothe[];
 	int result;
+	
+	private static SRM637_GreaterGameDiv2 greaterGameDiv2 = new SRM637_GreaterGameDiv2();
 
 	@Parameters
 	public static List<Object []> data() {
 		return Arrays.asList( new Object[][] {
-			{ new int[] {1,3}, 			new int[] {4, 2},		1 },
-			{ new int[] {1,3,5,7,9},	new int[] {2,4,6,8,10}, 0 },
-			{ new int[] {2},			new int[] {1},			1}
+			{ new int[] {1,3}, 			new int[] {4, 2},		1},
+			{ new int[] {1,3,5,7,9},	new int[] {2,4,6,8,10}, 0},
+			{ new int[] {2},			new int[] {1},			1},
+			{ new int[] {1,3,5,7,9},	new int[] {2,4,6,8,10},	0},
+			{ new int[] {2},			new int[] {1},			1},
+			{ new int[] {3,5,9,16,14,20,15,17,13,2}, 
+										new int[] {6,18,1,8,7,10,11,19,12,4}, 
+																6}
 		});
 	}
-	
-	// TODO: Add test.
 	
 	public SRM637_GreaterGameDiv2Test(int [] cardListSnuke, int [] cardListSothe, int result) {
 		this.cardListSnuke = cardListSnuke;
 		this.cardListSothe = cardListSothe;
 		this.result = result;
 	}
-	
 
-/*	@Test
-	public void test2() {
-		int[] c = {1,3,5,7,9};
-		int[] d = {2,4,6,8,10};
-		assertTrue( calc(c, d) == 0);
-	}
-	
 	@Test
-	public void test3() {
-		int[] e = {2};
-		int[] f = {1};
-		assertTrue( calc(e, f) == 1);
+	public void greaterGameDiv2Test() {
+		assertEquals(result, greaterGameDiv2.calc(cardListSnuke, cardListSothe ));
 	}
-	
-	@Test
-	public void test4() {
-		int[] g = {3,5,9,16,14,20,15,17,13,2};
-		int[] h = {6,18,1,8,7,10,11,19,12,4};
-		assertTrue( calc(g, h) == 6);
-	}*/
 }
