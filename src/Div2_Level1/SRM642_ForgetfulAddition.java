571c6a30208d4bde6b34fdfa44dcefee640ebc79
haroldalmon <haroldemail@gmail.com>
2015-08-12 18:44:30 -0500

    Remove JUnit tests

--- src/Div2_Level1/SRM642_ForgetfulAddition.java
+++ src/Div2_Level1/SRM642_ForgetfulAddition.java
@@ -1,10 +1,7 @@
 package Div2_Level1;
 
-import static org.junit.Assert.assertTrue;
-import org.junit.Test;
-
 public class SRM642_ForgetfulAddition {
-	int minNumber( String expression ) {
+	public int minNumber( String expression ) {
 		int minimumSum = Integer.MAX_VALUE;
 		
 		for ( int digitIndex = 1; digitIndex < expression.length(); digitIndex++ ) {
@@ -22,31 +19,6 @@ public class SRM642_ForgetfulAddition {
 		}
 
 		return minimumSum;
-	}
-
-	@Test
-	public void test1() {
-		assertTrue(minNumber("22") == 4 );
-	}
-	
-	@Test
-	public void test2() {
-		assertTrue(minNumber("123") == 15 );
-	}
-	
-	@Test
-	public void test3() {
-		assertTrue(minNumber("1289") == 101 );
-	}
-	
-	@Test
-	public void test4() {
-		assertTrue(minNumber("31415926") == 9067 );
-	}
-
-	@Test
-	public void test5() {
-		assertTrue(minNumber("98765") == 863 );
 	}
 }
 
