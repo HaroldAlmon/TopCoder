74bc42d39bd7ce4d4d3daacda7154f9d4bfb09d5
haroldalmon <haroldemail@gmail.com>
2015-08-18 20:24:38 -0500

    Remove unused function

--- src/JUnitTests/SRM642_ForgetfulAdditionTest.java
+++ src/JUnitTests/SRM642_ForgetfulAdditionTest.java
@@ -20,26 +20,6 @@ public class SRM642_ForgetfulAdditionTest {
 	
 	private static SRM642_ForgetfulAddition forgetfulAddition = new SRM642_ForgetfulAddition();
 
-	public static int minNumber( String expression ) {
-		int minimumSum = Integer.MAX_VALUE;
-		
-		for ( int digitIndex = 1; digitIndex < expression.length(); digitIndex++ ) {
-			int int1;
-			int int2;
-			int total; 
-			
-			int1 = Integer.parseInt( expression.substring( 0, digitIndex) );
-			int2 = Integer.parseInt( expression.substring( digitIndex, expression.length() ) );
-
-			total = int1 + int2;
-			
-			if ( total < minimumSum )
-				minimumSum = total;
-		}
-
-		return minimumSum;
-	}
-	
 	@Parameters
 	public static Collection< Object [] > getData() {
 		return Arrays.asList(new Object[][] {
